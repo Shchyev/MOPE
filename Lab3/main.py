5752511b4f46307a47bf0ca94b6e80502f775a56
Shcuryk Yevhenii <61512801+Modizan@users.noreply.github.com>
2021-05-30 12:49:01 +0300

    Add files via upload

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,301 +1,116 @@
-import math
-import random
+from random import randrange
+import math as mt
 import numpy as np
-from tabulate import tabulate
-
-
-m = 3
+m = 5
+X = [[-1, 1, -1], [-1, -1, 1]]
 Y = []
 
-x1min = -20
-x1max = 15
+minX1 = -20
+maxX1 = 15
 
-x2min = -15
-x2max = 35
+minX2 = -15
+maxX2 = 35
 
-x3min = -15
-x3max = -10
+minY1 = -40
+maxY1 = 60
 
+# Generating Y
+for i in range(5):
+    Y.append([])
+    for j in range(3):
+        Y[i].append(randrange(minY1, maxY1+1))
 
-x0 = [1, 1, 1, 1]
-x1 = [-1, -1, 1, 1]
-x2 = [-1, 1, -1, 1]
-x3 = [-1, 1, 1, -1]
+# Full table
+table = []
+for i in range(3):
+    table.append([X[0][i], X[1][i], Y[0][i], Y[1][i], Y[2][i], Y[3][i], Y[4][i]])
 
-x = [x0, x1, x2, x3]
-X = [
-    [x1min, x1min, x1max, x1max],
-    [x2min, x2max, x2min, x2max],
-    [x3min, x3max, x3max, x3min],
-]
-y_min = 200 + (x1min + x2min + x3min) / 3
-y_max = 200 + (x1max + x2max + x3max) / 3
+print("- "*29)
+print(" {:^5} {} {:^5} {} {:^5} {} {:^5} {} {:^5} {} {:^5} {} {:^5} {}".format("X1", "|", "X2", "|", "Y1", "|", "Y2", "|", "Y3", "|", "Y4", "|", "Y5", "|"))
 
-y1 = []
-y2 = []
-y3 = []
+for i in range(3):
+    for j in range(7):
+        print("{:>6.1f}".format(table[i][j]), end=" |")
+    print("\t")
 
-for i in range(4):
-    y1.append(random.randint(int(y_min), int(y_max)))
-    y2.append(random.randint(int(y_min), int(y_max)))
-    y3.append(random.randint(int(y_min), int(y_max)))
+# Average Y
+avgY1 = sum([elem[0] for elem in Y])/5
+avgY2 = sum([elem[1] for elem in Y])/5
+avgY3 = sum([elem[2] for elem in Y])/5
 
-Y.append(y1)
-Y.append(y2)
-Y.append(y3)
+# Dispersion
+dis1 = sum([((elem[0] - avgY1) ** 2) for elem in Y])/5
+dis2 = sum([((elem[1] - avgY2) ** 2) for elem in Y])/5
+dis3 = sum([((elem[2] - avgY3) ** 2) for elem in Y])/5
 
-# Show X and Y values as a table
-table = list()
-table.append([x[0] for x in X] + [x[0] for x in Y])
-table.append([x[1] for x in X] + [x[1] for x in Y])
-table.append([x[2] for x in X] + [x[2] for x in Y])
-table.append([x[3] for x in X] + [x[3] for x in Y])
-print(
-    tabulate(
-        table,
-        headers=["X1", "X2", "X3", "Y1", "Y2", "Y3"],
-        floatfmt=".4f",
-        tablefmt="fancy_grid",
-    )
-)
-
-# Calculate average for every row
-y1avg = sum([x[0] for x in Y]) / 3
-y2avg = sum([x[1] for x in Y]) / 3
-y3avg = sum([x[2] for x in Y]) / 3
-y4avg = sum([x[3] for x in Y]) / 3
-yavg = [y1avg, y2avg, y3avg, y4avg]
+# Deviation
+deviation = mt.sqrt((2 * (2 * m - 2)) / (m * (m - 4)))
 
 
-def generate_random_y(ymin, ymax):
+fuv1 = dis1/dis2 if dis1 >= dis2 else dis2/dis1
+fuv2 = dis3/dis1 if dis3 >= dis1 else dis1/dis3
+fuv3 = dis3/dis2 if dis3 >= dis2 else dis2/dis3
 
-    for i in range(5):
-        Y.append(
-            [
-                random.randint(ymin, ymax),
-                random.randint(ymin, ymax),
-                random.randint(ymin, ymax),
-            ]
-        )
+sigmaUv1 = ((m - 2) / m) * fuv1
+sigmaUv2 = ((m - 2) / m) * fuv2
+sigmaUv3 = ((m - 2) / m) * fuv3
 
-def generate_test_y():
+ruv1 = abs(sigmaUv1 - 1) / deviation
+ruv2 = abs(sigmaUv2 - 1) / deviation
+ruv3 = abs(sigmaUv3 - 1) / deviation
 
-    # You can use an example Y values. Just set test_y variable to True when creating Lab2 object
-    Y.append([9, 15, 20])
-    Y.append([10, 14, 18])
-    Y.append([11, 10, 12])
-    Y.append([15, 12, 10])
-    Y.append([9, 14, 16])
+print("\n" + "- "*29)
+print("Average Y1: {}\nAverage Y2: {}\nAverage Y3: {}".format(avgY1, avgY2, avgY3))
 
+print("\n" + "- "*29)
+print("Dispersion 1: {}\nDispersion 2: {}\nDispersion 3: {}".format(dis1, dis2, dis3))
 
-# Normalize regression factors
+print("\n" + "- "*29)
+print("Deviation: {}".format(deviation))
 
-mx1 = sum(X[0]) / 4
-mx2 = sum(X[1]) / 4
-mx3 = sum(X[2]) / 4
-my = (y1avg + y2avg + y3avg + y4avg) / 4
-a1 = (
-             X[0][0] * y1avg
-             + X[0][1] * y2avg
-             + X[0][2] * y3avg
-             + X[0][3] * y4avg
-) / 4
-a2 = (
-             X[1][0] * y1avg
-             + X[1][1] * y2avg
-             + X[1][2] * y3avg
-             + X[1][3] * y4avg
-) / 4
-a3 = (
-             X[2][0] * y1avg
-             + X[2][1] * y2avg
-             + X[2][2] * y3avg
-             + X[2][3] * y4avg
-) / 4
+print("\n" + "- "*29)
+print("Fuv 1: {}\nfuv 2: {}\nfuv 3: {}".format(fuv1, fuv2, fuv3))
 
-a11 = sum([x ** 2 for x in X[0]]) / 4
-a22 = sum([x ** 2 for x in X[1]]) / 4
-a33 = sum([x ** 2 for x in X[2]]) / 4
+print("\n" + "- "*29)
+print("Sigma 1: {}\nSigma 2: {}\nSigma 3: {}".format(sigmaUv1, sigmaUv2, sigmaUv3))
 
-a12 = sum([X[0][i] * X[1][i] for i in range(4)]) / 4
-a13 = sum([X[0][i] * X[2][i] for i in range(4)]) / 4
-a23 = sum([X[1][i] * X[2][i] for i in range(4)]) / 4
+print("\n" + "- "*29)
+print("Ruv 1: {}\nRuv 2: {}\nRuv 3: {}".format(ruv1, ruv2, ruv3))
 
-a21 = a12
-a31 = a13
-a32 = a23
-
-# Calculating determinants of matrixes
-b0 = np.linalg.det(
-    [
-        [my, mx1, mx2, mx3],
-        [a1, a11, a12, a13],
-        [a2, a12, a22, a32],
-        [a3, a13, a23, a33],
-    ]
-) / np.linalg.det(
-    [
-        [1, mx1, mx2, mx3],
-        [mx1, a11, a12, a13],
-        [mx2, a12, a22, a32],
-        [mx3, a13, a23, a33],
-    ]
-)
-
-b1 = np.linalg.det(
-    [
-        [1, my, mx2, mx3],
-        [mx1, a1, a12, a13],
-        [mx2, a2, a22, a32],
-        [mx3, a3, a23, a33],
-    ]
-) / np.linalg.det(
-    [
-        [1, mx1, mx2, mx3],
-        [mx1, a11, a12, a13],
-        [mx2, a12, a22, a32],
-        [mx3, a13, a23, a33],
-    ]
-)
-
-b2 = np.linalg.det(
-    [
-        [1, mx1, my, mx3],
-        [mx1, a11, a1, a13],
-        [mx2, a12, a2, a32],
-        [mx3, a13, a3, a33],
-    ]
-) / np.linalg.det(
-    [
-        [1, mx1, mx2, mx3],
-        [mx1, a11, a12, a13],
-        [mx2, a12, a22, a32],
-        [mx3, a13, a23, a33],
-    ]
-)
-
-b3 = np.linalg.det(
-    [
-        [1, mx1, mx2, my],
-        [mx1, a11, a12, a1],
-        [mx2, a12, a22, a2],
-        [mx3, a13, a23, a3],
-    ]
-) / np.linalg.det(
-    [
-        [1, mx1, mx2, mx3],
-        [mx1, a11, a12, a13],
-        [mx2, a12, a22, a32],
-        [mx3, a13, a23, a33],
-    ]
-)
-b = [b0, b1, b2, b3]
-print(
-    "Regression equation: y = {:.3f} + {:.3f}x1 + {:.3f}x2 + {:.3f}x3".format(
-        b0, b1, b2, b3
-    )
-)
-
-print("\nCochran test:")
-
-# Cochran
-
-# Calculate dispersions for every row
-d1 = sum([((x[0] - y1avg) ** 2) for x in Y]) / 3
-d2 = sum([((x[1] - y2avg) ** 2) for x in Y]) / 3
-d3 = sum([((x[2] - y3avg) ** 2) for x in Y]) / 3
-d4 = sum([((x[3] - y4avg) ** 2) for x in Y]) / 3
-d = [d1, d2, d3, d4]
-print("Dispersions:")
-for i in range(4):
-    print("d{} = {:.3f}".format(i + 1, d[i]))
-gp = max(d) / sum(d)
-print("\ngp = {:.3f}".format(gp))
-if gp < 0.7679:
-    print("✅Cochran’s C test passed")
+print("\n" + "- "*29)
+print("Check of dispersion`s uniformity is..")
+if ruv1 <= 2 and ruv2 <= 2 and ruv3 <= 2:
+    print("Successful")
 else:
-    print("❌Cochran’s C test failed")
+    print("Failed")
 
-print("\nStudent's t-test")
+mx1 = sum(X[0])/3
+mx2 = sum(X[1])/3
+my = (avgY1 + avgY2 + avgY3)/3
+a1 = sum([elem ** 2 for elem in X[0]])/3
+a2 = ((X[0][0] * X[1][0]) + (X[0][1] * X[1][1]) + (X[0][2] * X[1][2]))/3
+a3 = sum([elem ** 2 for elem in X[1]])/3
+a11 = (X[0][0] * avgY1 + X[0][1] * avgY2 + X[0][2] * avgY3)/3
+a22 = (X[1][0] * avgY1 + X[1][1] * avgY2 + X[1][2] * avgY3)/3
 
-# Student
+# Determinants
+b0 = np.linalg.det([[my, mx1, mx2], [a11, a1, a2], [a22, a2, a3]])/np.linalg.det(
+    [[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])
+b1 = np.linalg.det([[1, my, mx2], [mx1, a11, a2], [mx2, a22, a3]])/np.linalg.det(
+    [[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])
+b2 = np.linalg.det([[1, mx1, my], [mx1, a1, a11], [mx2, a2, a22]])/np.linalg.det(
+    [[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])
 
-N = len(d)
-N = N
-sbsq = sum(d) / N
-sbsq = sbsq
-sbssq = sbsq / (m * N)
-sbs = math.sqrt(sbssq)
+print("\n" + "- "*29)
+print("Normalized regression equation: y = {:.1f} + {:.1f}x1 + {:.1f}x2".format(b0, b1, b2))
 
-print("Sb = {:.3f}".format(sbs))
+dx1 = abs(maxX1 - minX1)/2
+dx2 = abs(maxX2 - minX2)/2
+x10 = (maxX1 + minX1)/2
+x20 = (maxX2 + minX2)/2
+a0 = b0 - b1 * (x10 / dx1) - b2 * (x20/dx2)
+a1 = b1/dx1
+a2 = b2/dx2
 
-b0 = sum([yavg[i] * x[0][i] for i in range(0, N)]) / N
-b1 = sum([yavg[i] * x[1][i] for i in range(0, N)]) / N
-b2 = sum([yavg[i] * x[2][i] for i in range(0, N)]) / N
-b3 = sum([yavg[i] * x[3][i] for i in range(0, N)]) / N
-
-b = [b0, b1, b2, b3]
-t = [abs(x) / sbs for x in b]
-
-print("Beta:")
-for i in range(4):
-    print("b{} = {:.3f}".format(i, b[i]))
-
-print("\nt:")
-for i in range(4):
-    print("t{} = {:.3f}".format(i, t[i]))
-
-print()
-
-t_tabl = 2.306
-regr_eq = ""
-nm = []
-xs = ["x1", "x2", "x3"]
-d = 0
-
-yd = [0, 0, 0, 0]
-for i in range(4):
-    if t[i] < t_tabl:
-        nm.append(i)
-        d += 1
-    else:
-        for j in range(4):
-            if i == 0:
-                yd[j] += b[i]
-            else:
-                yd[j] += b[i] * X[2][j]
-        if i == 0:
-            regr_eq += "{:.3f} + ".format(b[0])
-        else:
-            regr_eq += "{:.3f}*{} + ".format(b[i], xs[i - 1])
-if len(regr_eq) != 0:
-    regr_eq = regr_eq[0:-2]
-
-nmt = ",".join(["t" + str(x) for x in nm])
-nmb = ",".join(["b" + str(x + 1) for x in nm])
-print("{} < t_tabl(t_tabl=2.306)".format(nmt))
-print("Factors {} can be excluded".format(nmb))
-print("Regression equation without excluded factors:")
-print("y = {}\n".format(regr_eq))
-
-for i in range(4):
-    print("y{} = {:.3f}".format(i + 1, yd[i]))
-
-d = d
-yd = yd
-
-print("\nF-test")
-
-# Fisher crit
-print("d = {}".format(d))
-sadsq = (m / (N - d)) * sum(
-    [(yd[i] - yavg[i]) ** 2 for i in range(N)]
-)
-Fp = sadsq / sbsq
-print("S2ad = {:.3f}\nFp = {:.3f}".format(sadsq, Fp))
-table = [5.3, 4.5, 4.1, 3.8, 3.7, 3.6, 3.3, 3.1, 2.9]
-tablec = table[N - d - 1]
-if Fp < tablec:
-    print("✅F-test passed/model is adequate")
-else:
-    print("❌F-test failed/model is NOT adequate")
+print("\n" + "- "*29)
+print("Naturalized regression equation: y = {:.1f} + {:.1f}x1 + {:.1f}x2".format(a0, a1, a2))
